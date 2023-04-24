ae72bc4c7b79109fbf59ad408b0e4c9b714b46d7
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-04-24 14:32:09 -0400

    added basic functionality

---
+++ script.js
@@ -0,0 +1,72 @@
+const canvas = document.getElementById("canvas"); // Actual Canvas
+
+canvas.width = window.innerWidth - canvas.offsetLeft;
+
+canvas.height = window.innerHeight - canvas.offsetTop;
+
+const ctx = canvas.getContext("2d"); // canvas context
+
+const clearBtn = document.getElementById("clear-btn"); // clears canvas
+
+const lineWidth = document.getElementById("line-width"); // line-width input
+
+const colorSelector = document.getElementById("color-selector"); // color input
+
+// console.log(`canvas width${canvas.width}`);
+// console.log(`canvas height${canvas.height}`);
+// console.log(`canvas offsetX ${canvas.offsetLeft}`);
+// console.log(`canvas offsetY${canvas.offsetTop}`);
+
+//Global Variables:
+let isPainting = false;
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!isPainting) {
+    return;
+  }
+  ctx.strokeStyle = colorSelector.value;
+  ctx.lineWidth = lineWidth.value;
+  ctx.lineCap = "round";
+  ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+  ctx.stroke();
+});
+
+// Function: Clear canvas
+
+clearBtn.addEventListener("click", (e) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  isPainting = false;
+});
+
+// Function udpate color
+colorSelector.addEventListener("change", (e) => {
+  if (e.target.id === "color-selector") {
+    ctx.strokeStyle = e.target.value;
+  }
+});
+
+// Function update Line-Width
+lineWidth.addEventListener("input", (e) => {
+  if (e.target.id === "line-width") {
+    ctx.lineWidth = e.target.value;
+  }
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  isPainting = true;
+  ctx.beginPath();
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPainting = false;
+});
+window.addEventListener("mouseup", (e) => {
+  isPainting = false;
+  ctx.beginPath();
+});
+
+// Function to stop drawing if the mouse moved outside of the canvas
+canvas.addEventListener("blur", (e) => {
+  isPainting = false;
+  ctx.closePath();
+});
